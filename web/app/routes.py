a7736cb098b55f88bcb93826d1f4aaf2e522b1c7
Doaaxv <35659577+Doaaxv@users.noreply.github.com>
2023-01-09 22:48:38 +0300

    installed dependencies

--- web/app/routes.py
+++ web/app/routes.py
@@ -2,7 +2,9 @@ from app import app, db, queue_client
 from datetime import datetime
 from app.models import Attendee, Conference, Notification
 from flask import render_template, session, request, redirect, url_for, flash, make_response, session
-from azure.servicebus import Message
+# from azure.servicebus import Message
+# from azure.servicebus import ServiceBusClient 
+# https://pypi.org/project/azure-servicebus/
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 import logging
